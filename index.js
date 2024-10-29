f3e0dd168858c8a47bb9c44f8e39421e111344f2
genuino12 <lucasgenuino567@gmail.com>
2024-10-29 19:59:31 -0300

    atualizações

--- index.js
+++ index.js
@@ -4,38 +4,24 @@ const host = '127.0.0.1';
 const porta = 4000;
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use(express.static('./publico'));
 app.use(express.static('./privado'));
 
-let interessados = []; 
-let filhotes = []; 
 
-// Rota para cadastrar interessados
-app.post('/interessados', (req, res) => {
-    const interessado = req.body; 
-    interessados.push(interessado); 
-    res.json({ message: 'Interessado registrado', interessado });
-});
-
-// Rota para buscar todos os interessados
 app.get('/interessados', (req, res) => {
-    res.json(interessados); 
+    res.json({ message: 'Rota para buscar interessados' });
 });
 
-// Rota para cadastrar filhotes
 app.post('/filhotes', (req, res) => {
-    const filhote = req.body; 
-    filhotes.push(filhote); 
-    res.json({ message: 'Filhote registrado', filhote });
+    const tipo = req.body;
+    res.json({ message: 'Filhote registrado', tipo });
 });
 
-
-app.get('/filhotes', (req, res) => {
-    res.json(filhotes); 
-});
 
 app.listen(porta, host, () => {
     console.log(`Servidor iniciado em http://${host}:${porta}`);
